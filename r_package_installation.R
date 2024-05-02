fe24780a9f08fd743a91d70c403384f7b4f7b921
caitlinobc <caitlinobc@gmail.com>
2024-05-02 11:54:32 -0700

    Create r_package_installation.R

---
+++ r_package_installation.R
@@ -0,0 +1,54 @@
+# ----------------------------------------------
+# Caitlin O'Brien-Carelli
+#
+# 3/11/2024
+# List of packages to install to set up R
+# To run my code, you need all of these packages (install once)
+# This list should include every package you need
+# Some packages are a bit outdated - warnings are OK
+# ----------------------------------------------
+# CHANGE THIS
+
+
+# ------------------------
+# install packages to use in R
+
+# packages to import and export data
+install.packages('openxlsx')
+install.packages('readxl')
+install.packages('writexl')
+install.packages('xlsx')
+install.packages('haven') #import sas files
+
+# key packages for munging and visualization
+install.packages('data.table')
+install.packages('lubridate')
+install.packages('tidyr')
+install.packages('plyr')
+install.packages('dplyr')
+install.packages('zoo')
+install.packages('stringr')
+install.packages('ggplot2')
+install.packages('RColorBrewer')
+install.packages('gridExtra')
+install.packages('epitools')
+install.packages('eeptools')
+install.packages('Hmisc')
+install.packages('rgdal')
+install.packages('eeptools')
+
+# mapping packages
+install.packages('Rtools')
+install.packages('maptools')
+install.packages('raster')
+install.packages('rgdal')
+install.packages('rgeos')
+install.packages('broom')
+install.packages('ggrepel')
+
+# webscrape packages
+install.packages('jsonlite')
+install.packages('httr')
+install.packages('RJSONIO')
+
+# ------------------------
